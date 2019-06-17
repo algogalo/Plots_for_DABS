41f713f04abbe350648b270dd857d3ab91f58e05
Galo <grivera92@gmail.com>
2019-06-17 14:03:57 +0200

    imported data and slected data frame to use

--- DABS_plots_codes.py
+++ DABS_plots_codes.py
@@ -4,3 +4,5 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 
 Raw_Data = pd.read_csv("rawdata.csv")
+
+df = Raw_Data.loc[:,["Experiment", "Replicate", "Toxin", "Time_of_Exposure", "Mosquitoes"]]
